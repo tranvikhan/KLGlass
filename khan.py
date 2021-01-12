9871b69fd595f06f7513d441a10ccdd123aa18de
tranvikhan <tranvikhan@gmail.com>
2021-01-12 10:51:00 +0700

    add new face

--- khan.py
+++ khan.py
@@ -1,4 +1,5 @@
 from PyQt5 import QtWidgets, uic
+from PyQt5.QtCore import Qt
 import sys
 import time
 
@@ -6,14 +7,16 @@ import time
 class Ui(QtWidgets.QMainWindow):
     def __init__(self):
         super(Ui, self).__init__()
-        uic.loadUi('./UI/GlaasFace1.ui', self)
-        self.Gio1 = self.findChild(QtWidgets.QLabel, 'Gio1')  # Find the button
+        uic.loadUi('/home/pi/KLGlass/UI/demo.ui', self)
+        # self.Gio1 = self.findChild(QtWidgets.QLabel, 'Gio1')  # Find the button
         # Remember to pass the definition/method, not the return value!
+        self.setWindowFlag(Qt.FramelessWindowHint)
+        self.showMaximized()
         self.show()
 
     def loadding(self):
         print('printButtonPressed')
-        self.Gio1.setText('khan')
+        # self.Gio1.setText('khan')
         # self.Gio1.setText(str(i))
 
 
