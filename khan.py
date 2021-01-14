8c5eb6e4ba4d5c27345940910100ee113988a027
tranvikhan <tranvikhan@gmail.com>
2021-01-14 22:22:50 +0700

    oke updatetime

--- khan.py
+++ khan.py
@@ -1,5 +1,5 @@
 from PyQt5 import QtWidgets, uic
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import QTimer, QTime, Qt, QDate
 import sys
 from bluedot.btcomm import BluetoothServer
 
@@ -10,21 +10,34 @@ strPc = '.'
 class Ui(QtWidgets.QMainWindow):
     def __init__(self):
         super(Ui, self).__init__()
-        uic.loadUi(strLocal+'/UI/MatChinh.ui', self)
-        self.SpeedText = self.findChild(
-            QtWidgets.QLabel, 'Speed')  # Find the button
-        # Remember to pass the definition/method, not the return value!
-        # self.setWindowFlag(Qt.FramelessWindowHint)
-        self.showMaximized()
-        self.show()
+        uic.loadUi(strPc+'/UI/MatChinh.ui', self)
+        self.TimeTxt = self.findChild(QtWidgets.QLabel, 'Time')
+        self.DateTxt = self.findChild(QtWidgets.QLabel, 'Ngay')
+        timer = QTimer(self)
+        timer.timeout.connect(self.showTime)
+        timer.start(1000)
 
         def data_received(data):
             print(data)
-            self.SpeedText.setText(data)
+            self.TimeTxt.setText(data)
             s.send(data)
         s = BluetoothServer(data_received)
+
+    def showTime(self):
+        # getting current time
+        current_time = QTime.currentTime()
+        current_Date = QDate.currentDate()
+
+        label_date = current_Date.toString('dd.MM.yyyy')
+        # converting QTime object to string
+        label_time = current_time.toString('hh:mm:ss')
+        # showing it to the label
+        self.TimeTxt.setText(label_time)
+        self.DateTxt.setText(label_date)
 
 
 app = QtWidgets.QApplication(sys.argv)
 window = Ui()
+window.showMaximized()
+window.show()
 app.exec_()
